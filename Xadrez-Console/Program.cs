aa6eea20c3b03e59f975f50eeb56112a0705168f
Ismael Lunkes <lunkes86@gmail.com>
2019-11-02 18:56:57 -0300

    Criando a classe posição xadrez

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -8,21 +8,27 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
-                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(1, 3));
-                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
+            /*  try
+              {
+                  Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+                  tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+                  tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+                  tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(1, 3));
+                  tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
 
-                Tela.imprimirTabuleiro(tabuleiro);
+                  Tela.imprimirTabuleiro(tabuleiro);
 
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+              }
+              catch (TabuleiroException e)
+              {
+                  Console.WriteLine(e.Message);
+              }*/
+
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            pos.toPosicao();
+
+            Console.Write(pos);
+
             Console.ReadLine();
         }
     }
---
+++ Xadrez-Console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+﻿using Xadrez_Console.tabuleiro;
+
+namespace Xadrez_Console.xadrez
+{
+    class PosicaoXadrez
+    {
+
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;         
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
