ecaaf6bfea9427776b23c9e8f05d6c9a14f1062d
Ismael Lunkes <lunkes86@gmail.com>
2019-11-02 19:09:15 -0300

    Melhorando o visual do tabuleiro diferenciando. Tecnica usada: Elementos Estáticos!

--- Xadrez-Console/Tela.cs
+++ Xadrez-Console/Tela.cs
@@ -9,6 +9,7 @@ namespace Xadrez_Console
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -17,11 +18,31 @@ namespace Xadrez_Console
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
+                        //Console.Write(tab.peca(i, j) + " ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");            
+
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+
         }
     }
 }
